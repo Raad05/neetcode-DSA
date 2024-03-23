31b9e545191845b3116f790e3339888061878e9b
Raad05 <ashabulraad@gmail.com>
2024-03-23 23:43:07 +0600

    linear and binary search problem 1 - more efficient

--- Lesson 01/Linear and Binary Search/problem01.py
+++ Lesson 01/Linear and Binary Search/problem01.py
@@ -1,5 +1,4 @@
-
-# Linear Search (Bruteforce method) => O(n)
+# # Linear Search (Bruteforce method) => O(n)
 # def locate_card(cards, target):
 #     for i in cards:
 #         if i == target:
@@ -7,22 +6,58 @@
 
 #     return False
 
-# Binary Search => O(logn)
+# arr = [10, 9, 8, 7, 6, 5, 4, 3]
+# print(locate_card(arr, 11))
+
+# # Binary Search => O(logn)
+# def locate_card(cards, target):
+#     lo, hi = 0, len(cards) - 1
+
+#     while lo <= hi:
+#         mid = (lo + hi) // 2
+#         mid_number = cards[mid]
+
+#         if mid_number == target:
+#             return True
+#         elif mid_number < target:
+#             hi = mid - 1
+#         elif mid_number > target:
+#             lo = mid + 1
+
+#     return False
+
+# arr = [10, 9, 8, 7, 6, 5, 4, 3]
+# print(locate_card(arr, 11))
+
+# More efficient
+def test_location(cards, target, mid):
+    mid_number = cards[mid]
+
+    if mid_number == target:
+        if mid - 1 >= 0 and cards[mid - 1] == target:
+            return 'left'
+        else:
+            return 'found'
+    elif mid_number < target:
+        return 'left'
+    elif mid_number > target:
+        return 'right'
+
 def locate_card(cards, target):
     lo, hi = 0, len(cards) - 1
 
     while lo <= hi:
         mid = (lo + hi) // 2
-        mid_number = cards[mid]
+        result = test_location(cards, target, mid)
 
-        if mid_number == target:
+        if result == 'found':
             return True
-        elif mid_number < target:
+        elif result == 'left':
             hi = mid - 1
-        elif mid_number > target:
+        elif result == 'right':
             lo = mid + 1
 
     return False
 
-arr = [10, 9, 8, 7, 6, 5, 4, 3]
+arr = [10, 9, 9, 9, 7, 7, 7, 7, 6]
 print(locate_card(arr, 11))
