d8220a65af3aa4e7eb0d864f8d63c6e37feb65d8
Raad05 <ashabulraad@gmail.com>
2024-04-14 22:06:40 +0600

    leetcode problem_26 solved

---
+++ Arrays/problem_26.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        count = 0
+        for i in range(len(nums)):
+            if i < len(nums) - 2 and nums[i] == nums[i + 1]:
+                continue
+            nums[count] = nums[i]
+            count += 1
+
+        return count
+        
+# nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
