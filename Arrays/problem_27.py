3792b1f164e9d6bbe887ce90047e79248c946bf0
Raad05 <ashabulraad@gmail.com>
2024-04-16 13:58:48 +0600

    leetcode problem 27 solved

---
+++ Arrays/problem_27.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        count = 0
+        for i in range(len(nums)):
+            # if element matches with val, ignore it and move to next iteration
+            if nums[i] == val:
+                continue
+            # else, do in-place operation within the list and increase the number of count 
+            nums[count] = nums[i]
+            count += 1
+            
+        return count
+        
+# sample input list
+# nums = [3, 2, 2, 3]
