128e3c508cdada35356397c1566ec3749a7149ad
Raad05 <ashabulraad@gmail.com>
2024-05-31 22:46:47 +0600

    leetcode problem 700 solved without recursion

--- Trees/problem_700.py
+++ Trees/problem_700.py
@@ -4,15 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# with recursion
+# class Solution(object):
+#     def searchBST(self, root, val):
+#         if not root:
+#             return None
+        
+#         if val > root.val:
+#             return self.searchBST(root.right, val)
+#         elif val < root.val:
+#             return self.searchBST(root.left, val)
+#         else:
+#             return root
+
+# without recursion     
 class Solution(object):
     def searchBST(self, root, val):
-        if not root:
-            return None
-        
-        if val > root.val:
-            return self.searchBST(root.right, val)
-        elif val < root.val:
-            return self.searchBST(root.left, val)
-        else:
-            return root
-        
+        current = root
+
+        while current:
+            if val > current.val:
+                current = current.right
+            elif val < current.val:
+                current = current.left
+            else:
+                return current
