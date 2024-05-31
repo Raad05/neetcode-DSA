13b7e5ab0330a288f483978c54be0228dda74dd3
Raad05 <ashabulraad@gmail.com>
2024-05-31 23:12:36 +0600

    leetcode problem 235 solved with recursion

--- Trees/problem_235.py
+++ Trees/problem_235.py
@@ -5,6 +5,7 @@
 #         self.left = None
 #         self.right = None
 
+# without recursion
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
         current = root
@@ -16,3 +17,14 @@ class Solution(object):
                 current = current.right
             else:
                 return current
+            
+# with recursion
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+  
